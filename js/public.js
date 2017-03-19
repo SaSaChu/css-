94ad363dd12be8354a8847ecd47b8a62dd0f4b68
OA Wu <comdan66@gmail.com>
2017-03-19 23:51:21 +0800

    完成 demo_content 是亂文為了拉長網頁

--- js/public.js
+++ js/public.js
@@ -4,5 +4,23 @@
  */
 
 $(function () {
+  var menu_top = $('#menu').offset ().top;
+  $(window).scroll (function () {
+    
+    if ($(window).scrollTop () > menu_top) {
+      $('#menu').addClass ('fix');
+    } else {
+      $('#menu').removeClass ('fix');
+    }
+
+    if (!$('#icons').hasClass ('show') && ($(window).scrollTop () + $(window).height ()) > $('#icons').offset ().top) {
+      console.error ('x');
+      $('#icons').addClass ('show');
+    }
+    if (!$('#box').hasClass ('show') && ($(window).scrollTop () + $(window).height ()) > $('#box').offset ().top) {
+      $('#box').addClass ('show');
+    }
+    
+  });
 
 });
